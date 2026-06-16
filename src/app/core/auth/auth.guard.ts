cba7da5821ff126eac8bc7f7c446c93287900c91
Raúl Bustamante <raul@bustamante.me>
2026-06-16 18:37:24 -0500

    feat(auth): agregar authGuard funcional

---
+++ src/app/core/auth/auth.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthStore } from './auth.store';
+
+export const authGuard: CanActivateFn = () => {
+  const auth = inject(AuthStore);
+  const router = inject(Router);
+  return auth.estaAutenticado() ? true : router.createUrlTree(['/auth/login']);
+};
