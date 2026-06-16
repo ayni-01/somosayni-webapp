d1da1dfa051d6ae02012e7d350aad921029780f0
Raúl Bustamante <raul@bustamante.me>
2026-06-16 18:34:42 -0500

    feat(config): agregar environments por microservicio

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  api: {
+    identidad: 'http://localhost:8081/api/v1',
+    perfiles: 'http://localhost:8082/api/v1',
+    retos: 'http://localhost:8083/api/v1',
+    postulaciones: 'http://localhost:8084/api/v1',
+    habilidades: 'http://localhost:8085/api/v1',
+    notificaciones: 'http://localhost:8086/api/v1',
+    metricas: 'http://localhost:8087/api/v1',
+  },
+};
